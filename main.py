99d9f503dc5b8ab4fdcc85723934afd852139a7e
Abass Hassan <Ahassan124@student.cscc.edu>
2026-07-14 16:27:07 -0400

    Intial project set

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+Program: Video Game Collection Manager
+Author: Abass Hassan
+Purpose: Main program file for the video game collection manager.
+Resources: Python Materials
+Date: July 2026
+"""
+
+
+def display_welcome():
+    """
+    Display the welcome message for the program.
+    """
+
+    print("==============================")
+    print(" Video Game Collection Manager ")
+    print("==============================")
+
+
+def main():
+    """
+    Run the main program.
+    """
+
+    display_welcome()
+
+    print("Program starting...")
+
+
+if __name__ == "__main__":
+    main()
